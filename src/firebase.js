6a936c370373a0e57ab530b8f2d9e32fc8aaa821
SlavaChekhunov <slava97@my.yorku.ca>
2022-04-27 22:12:17 -0400

    added the firebase component

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtghzn_pZx1U5q9YpK5uebzeYCCYAYew0",
+  authDomain: "blog-24545.firebaseapp.com",
+  projectId: "blog-24545",
+  storageBucket: "blog-24545.appspot.com",
+  messagingSenderId: "954297140505",
+  appId: "1:954297140505:web:9e2c332a633f4689557188",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
